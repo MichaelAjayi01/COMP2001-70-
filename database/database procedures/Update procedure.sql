63daa4c82ccfc1db5e8097e900e74e5e4d6f7fac
Michael Ajayi <44640349+MichaelAjayi01@users.noreply.github.com>
2023-12-27 16:10:29 +0000

    Create Update procedure.sql
    update procedure created.

---
+++ database/database procedures/Update procedure.sql
@@ -0,0 +1,35 @@
+-- Update procedure for CW2_USER_PROFILE
+CREATE PROCEDURE UpdateUserProfile
+    @User_ID INT,
+    @First_Name NVARCHAR(50),
+    @Last_Name NVARCHAR(50),
+    @About NVARCHAR(MAX),
+    @Location NVARCHAR(100),
+    @Units NVARCHAR(20),
+    @Calorie_Counter_Info NVARCHAR(100),
+    @Height FLOAT,
+    @Weight FLOAT,
+    @Birthday DATE,
+    @Set_Password NVARCHAR(50),
+    @Profile_Picture VARBINARY(MAX)
+AS
+BEGIN
+    UPDATE CW2_USER_PROFILE
+    SET
+        First_Name = @First_Name,
+        Last_Name = @Last_Name,
+        About = @About,
+        Location = @Location,
+        Units = @Units,
+        Calorie_Counter_Info = @Calorie_Counter_Info,
+        Height = @Height,
+        Weight = @Weight,
+        Birthday = @Birthday,
+        Set_Password = @Set_Password,
+        Profile_Picture = @Profile_Picture
+    WHERE
+        User_ID = @User_ID;
+
+    INSERT INTO CW2_Audit_Log (User_ID, Operation_Type, Operation_DateTime, Operation_Details)
+    VALUES (@User_ID, 'UPDATE', GETDATE(), 'User profile updated.');
+END;
