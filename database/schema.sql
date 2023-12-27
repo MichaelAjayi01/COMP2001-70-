cdcc01a111344826ef4ba3ba17a1f4b6fb9e1094
Michael Ajayi <44640349+MichaelAjayi01@users.noreply.github.com>
2023-12-27 15:19:08 +0000

    Update schema.sql
    final db schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,6 @@
--- Fixed a lot of discrepancies between the diagram and SQL
-
 -- Creating CW2_User_Profile table
 CREATE TABLE CW2_USER_PROFILE (
-    User_ID INT IDENTITY(1,1) PRIMARY KEY,
+    User_ID INT IDENTITY(1,1) PRIMARY KEY, --
     First_Name NVARCHAR(50),
     Last_Name NVARCHAR(50),
     About NVARCHAR(MAX),
@@ -17,58 +15,57 @@ CREATE TABLE CW2_USER_PROFILE (
 );
 
 -- Creating CW2_Trails table
-CREATE TABLE CW2_Trails (
+CREATE TABLE CW2_Trails (--
     Trail_ID INT IDENTITY(1,1) PRIMARY KEY,
     Trail_Name NVARCHAR(100),
     List_of_Trails NVARCHAR(MAX)
 );
 
--- Creating CW2 completed trails table
-CREATE TABLE CW2_COMPLETED_TRAILS (
+-- Creating CW2_UserProfile_CompletedTrails_JT(Junction Table)
+CREATE TABLE CW2_UserProfile_CompletedTrails_JT ( --
+    User_ID INT,
+    Trail_ID INT,
+    PRIMARY KEY (User_ID, Trail_ID),
+    FOREIGN KEY (User_ID) REFERENCES CW2_USER_PROFILE(User_ID),
+    FOREIGN KEY (Trail_ID) REFERENCES CW2_Trails(Trail_ID)
+);
+
+-- Creating CW2_COMPLETED_TRAILS table
+CREATE TABLE CW2_COMPLETED_TRAILS (--
     User_ID INT,
     Trail_ID INT,
     Completed_Trail_Count INT DEFAULT 0,
     PRIMARY KEY (User_ID, Trail_ID),
-    FOREIGN KEY(User_ID) REFERENCES CW2_UserProfile_CompletedTrials_JT(User_ID),
-    FOREIGN KEY(Trail_ID) REFERENCES CW2_Trails(Trail_ID)
+    FOREIGN KEY (User_ID, Trail_ID) REFERENCES CW2_UserProfile_CompletedTrails_JT(User_ID, Trail_ID)
 );
 
-
 -- Creating CW2_Stats table
-CREATE TABLE CW2_Stats (
+CREATE TABLE CW2_Stats (--
     Stats_ID INT IDENTITY(1,1) PRIMARY KEY,
     User_ID INT,
     FOREIGN KEY (User_ID) REFERENCES CW2_USER_PROFILE(User_ID)
 );
 
 -- Creating CW2_Trail_Review_JT(Junction Table)
-CREATE TABLE CW2_Trail_Review_JT (
+CREATE TABLE CW2_Trail_Review_JT (--
     Trail_Review_ID INT IDENTITY(1,1) PRIMARY KEY,
     Trail_ID INT,
     Review_ID INT,
-    FOREIGN KEY(Trail_ID) REFERENCES CW2_Trails(Trail_ID)
+    FOREIGN KEY (Trail_ID) REFERENCES CW2_Trails(Trail_ID)
 );
 
--- Creating CW2_Trail_Reviews
-CREATE TABLE CW2_Reviews (
+-- Creating CW2_Reviews
+CREATE TABLE CW2_Reviews (--
     Review_ID INT IDENTITY(1,1) PRIMARY KEY,
     Trail_Review_ID INT,
     User_ID INT,
-    FOREIGN KEY (Trail_Review_ID) REFERENCES CW2_COMPLETED_TRAILS_JT(Trail_Review_ID),
-    FOREIGN KEY (User_ID) REFERENCES CW2_User_Profile(User_ID)
+    FOREIGN KEY (Trail_Review_ID, User_ID) REFERENCES CW2_COMPLETED_TRAILS(User_ID, Trail_ID),
+    FOREIGN KEY (User_ID) REFERENCES CW2_USER_PROFILE(User_ID)
 );
 
 -- Creating CW2_Custom_Routes_Table
 CREATE TABLE CW2_Custom_RouteS (
     Custom_Route_ID INT IDENTITY(1,1) PRIMARY KEY,
     User_ID INT,
-    FOREIGN KEY (User_ID) REFERENCES CW2_USER_PROFILE
-);
-
--- Creating CW2_UserProfile_CompletedTrials_JT(Junction Table)
-CREATE TABLE CW2_UserProfile_CompletedTrials_JT (
-    User_ID INT,
-    Trail_ID INT,
-    FOREIGN KEY (Trail_ID) REFERENCES CW2_COMPLETED_TRAILS(Trail_ID),
-    FOREIGN KEY(User_ID) REFERENCES CW2_User_Profile(User_ID)
+    FOREIGN KEY (User_ID) REFERENCES CW2_USER_PROFILE(User_ID)
 );
